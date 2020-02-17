f2e0d0714f633d2aee99a8d0800ac5da30ea31c1
Aaron Barany <akb825@gmail.com>
2020-02-16 17:20:38 -0800

    Updated Mac build to use macOS 10.14.
    macOS 10.13 will be discontinued soon from Azure build pipelines.

--- tool/test/run-test.bat
+++ tool/test/run-test.bat
@@ -1,9 +1,10 @@
 @ECHO OFF
 set command=%1
+set command=%command:/=\%
 set args=%~2
 set exitCode=%3
 
-%command% %args%
+"%command%" %args%
 set realExitCode=%ERRORLEVEL%
 if %exitCode% NEQ %realExitCode% (
 	echo got exit code %realExitCode%; expected %exitCode%
